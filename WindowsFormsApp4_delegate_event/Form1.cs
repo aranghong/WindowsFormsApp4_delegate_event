2b917606d97398b1af8791d4eda112ae70788e5b
aranghong <har2129@yu.ac.kr>
2025-05-19 10:29:47 +0900

    Add project files.

---
+++ WindowsFormsApp4_delegate_event/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4_delegate_event
+{
+    public delegate void Notify();  //1.델리게이트 타입 정의
+    public delegate void Notify2(); //이벤트 사용을 위한 델리게이트
+
+    public partial class Form1 : Form
+    {
+        //2.메서드 정의
+        static void alarmMessage()
+        {
+            Console.WriteLine("알람 울림");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            alarm a = new alarm();
+            alarm2 a2 = new alarm2();   //event
+            
+            //4.델리게이트 변수에 메서드 할당
+            a.onring += alarmMessage;
+            a2.onring += alarmMessage;  //event
+
+
+            //5.실행
+            a.onring();
+            //a2.onring();    //event -> 컴파일 오류
+            a2.trigger();   //클래스 내부 메서드만 가능 -> 안전함
+
+        }
+
+        //3.클래스 정의
+        public class alarm
+        {
+            public Notify onring;   //델리게이트 타입의 변수 선언
+        }
+
+        //이벤트 클래스
+        public class alarm2
+        {
+            public event Notify2 onring;
+
+            public void trigger() 
+            {
+                if (onring != null)
+                {
+                    onring();
+                }
+            }
+
+        }
+
+        //버튼 클래스 내부
+        //public event EventHanler Click; -> 델리게이트 기반 이벤트
+        //button.Click += new EventHanler(button1_Click);
+        //button1_Click -> 이벤트 핸들러에 연결되는 델리게이트임
+
+        //이벤트 핸들러 -> .NET에서 미리 정의된 델리게이트 타입
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp4_delegate_event/Form2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp4_delegate_event
+{
+
+    public delegate void EventDelegate();
+
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            EventManager e = new EventManager();
+            e.insert("hi", hi); //이벤트 추가
+            e.execution("hi");  //이벤트 실행
+            e.delete("hi", hi); //이벤트 삭제
+
+            e.insert("bye", bye);
+            e.execution("bye");
+
+
+        }
+
+        void hi()
+        {
+            //MessageBox.Show($"hi");
+            Console.WriteLine("hi");
+
+        }
+
+        void bye()
+        {
+            //MessageBox.Show($"bye");
+            Console.WriteLine("bye");
+        }
+
+    }
+
+    public class EventManager
+    {
+
+        private static Dictionary<string, EventDelegate> eventdic = new Dictionary<string, EventDelegate>();
+        public event EventDelegate onring;
+
+        //이벤트 추가
+        public void insert(string name, EventDelegate method)
+        {
+            eventdic[name] = method;
+            Console.WriteLine("이벤트 추가");
+        }
+
+        //이벤트 삭제
+        public void delete(string name, EventDelegate method)
+        {
+            if (eventdic.ContainsKey(name))
+            {
+                eventdic.Remove(name);
+                Console.WriteLine("이벤트 삭제");
+            }
+        }
+
+        //이벤트 실행
+        public void execution(string name)
+        {
+            if (eventdic.ContainsKey(name))
+            {
+                eventdic[name]?.Invoke();
+                Console.WriteLine("이벤트 실행");
+            }
+            else
+            {
+                //MessageBox.Show($"[{name}] 이벤트가 존재하지 않습니다.");
+                Console.WriteLine($"[{name}] 이벤트 존재하지 않음");
+            }
+        }
+
+    }
+
+}
